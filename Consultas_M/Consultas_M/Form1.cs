af4c03f7ee4c13e5c302e16c8510aec3fb97a4a7
caleb <calebymish@outlook.com>
2015-09-14 05:44:29 -0500

    rediseno
    asi es

--- Consultas_M/Consultas_M/Form1.cs
+++ Consultas_M/Consultas_M/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Consultas_M
 {
-    public partial class Form1 : Form
+    public partial class sCitas : Form
     {
-        public Form1()
+        public sCitas()
         {
             InitializeComponent();
         }
--- Consultas_M/Consultas_M/Program.cs
+++ Consultas_M/Consultas_M/Program.cs
@@ -16,7 +16,7 @@ namespace Consultas_M
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new sCitas());
         }
     }
 }
